487a8b32c793b8fa2f9728b4f152f6ec1b9268ea
SeanShi <seans@iqmetrix.com>
2015-12-24 11:27:05 -0600

    testing

--- android/app/src/main/java/com/example/seanshi/android/MainActivity.java
+++ android/app/src/main/java/com/example/seanshi/android/MainActivity.java
@@ -8,6 +8,8 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -24,6 +26,15 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
+            }
+        });
+
+        Button button = (Button)findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Perform action on click
+             //   _text.setText("button is clicked");
+                Toast.makeText(MainActivity.this, "Instant Run rocks...222.!", Toast.LENGTH_SHORT).show();
             }
         });
 
