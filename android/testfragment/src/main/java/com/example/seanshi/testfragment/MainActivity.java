81e7e4d3221a88b06fb7c0a9fc47f3920ba63d54
SeanShi <seans@iqmetrix.com>
2016-03-22 16:47:47 -0600

    test fragment

--- android/testfragment/src/main/java/com/example/seanshi/testfragment/MainActivity.java
+++ android/testfragment/src/main/java/com/example/seanshi/testfragment/MainActivity.java
@@ -3,16 +3,29 @@ package com.example.seanshi.testfragment;
 import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity implements PlusOneFragment.OnFragmentInteractionListener {
+import com.example.seanshi.testfragment.dummy.DummyContent;
+
+public class MainActivity extends AppCompatActivity implements PlusOneFragment.OnFragmentInteractionListener, ItemFragment.OnListFragmentInteractionListener {
+
+    TextView _text;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        _text = (TextView) findViewById(R.id.text);
     }
 
     public void onFragmentInteraction(Uri uri)
     {}
+
+
+    public void onListFragmentInteraction(DummyContent.DummyItem item)
+    {
+        _text.setText(item.toString());
+    }
 
 }
