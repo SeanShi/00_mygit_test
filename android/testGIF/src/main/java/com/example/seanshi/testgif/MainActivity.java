bea934687c667a5c6216ba6b4e80d84614cc572b
SeanShi <seans@iqmetrix.com>
2016-07-22 16:43:13 -0600

    testing

--- android/testGIF/src/main/java/com/example/seanshi/testgif/MainActivity.java
+++ android/testGIF/src/main/java/com/example/seanshi/testgif/MainActivity.java
@@ -2,6 +2,9 @@ package com.example.seanshi.testgif;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ImageView imageView = (ImageView) findViewById(R.id.my_image_view);
+        imageView.setImageResource(R.drawable.ic_launcher);
+        Glide.with(this).load(R.drawable.spinner).asGif()..into(imageView);
+
     }
 }
